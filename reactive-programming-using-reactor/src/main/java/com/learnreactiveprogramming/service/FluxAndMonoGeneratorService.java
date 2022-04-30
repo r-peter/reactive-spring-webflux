1235510ebf8e1320df37d64670df9ae73021f233
Richards Peter <richards.peter@outlook.com>
2022-04-30 01:40:13 -0400

    End of section 8

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,228 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"));
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("alex");
+    }
+
+    public Flux<String> namesFlux_map() {
+        return namesFlux().
+                //map(String::toUpperCase);
+                        map(s -> s.toUpperCase());
+    }
+
+    public Flux<String> namesFlux_immutability() {
+        Flux<String> namesFlux = namesFlux();
+        namesFlux.map(s -> s.toUpperCase());
+        return namesFlux;
+    }
+
+    public Flux<String> namesFlux_filter(int stringLength) {
+        return namesFlux()
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.length() > stringLength);
+    }
+
+    public Flux<String> namesFlux_filterMapPipeline(int stringLength) {
+        return namesFlux()
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.length() > stringLength)
+                .map(s -> s.length() + "-" + s);
+    }
+
+    public Flux<String> namesFlux_flatMap(int stringLength) {
+        return namesFlux()
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.length() > stringLength)
+                .flatMap(s -> splitString(s));
+    }
+
+    public Flux<String> namesFlux_flatMapAsync(int stringLength) {
+        Flux<String> names = Flux.fromIterable(List.of("gavaskar", "walsh", "robin"));
+
+        return names
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.length() > stringLength)
+                .flatMap(s -> splitStringWithDelay(s));
+    }
+
+    public Flux<String> namesFlux_ConcatMap(int stringLength) {
+        Flux<String> names = Flux.fromIterable(List.of("gavaskar", "walsh", "robin"));
+
+        return names
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.length() > stringLength)
+                .concatMap(s -> splitStringWithDelay(s));
+    }
+
+
+    public Mono<List<String>> namesMono_flatMap(int length) {
+        return Mono.just("alex")
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 3)
+                //.map(this::splitStringMono); - transformation returns another reactive type Mono. Hence, we have to flatten it.
+                .flatMap(this::splitStringMono);
+        //.flatMap(s -> splitStringMono(s)); - alternative way to write above code
+
+    }
+
+    public Flux<String> namesMono_flatMapMany(int length) {
+        return Mono.just("alex")
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 3)
+                //.map(this::splitString); - transformation returns another reactive type Flux. Hence, we have to flatten it.
+                .flatMapMany(this::splitString);
+        //.flatMapMany(s -> splitString(s));// - alternative way to write above code
+
+    }
+
+    public Flux<String> namesFlux_transform(int stringLength) {
+        Function<Flux<String>, Flux<String>> filterMap =
+                name -> name.map(s -> s.toUpperCase())
+                        .filter(s -> s.length() > stringLength);
+
+        return namesFlux()
+                /*
+                This function has been moved as Function Functional interface
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.length() > stringLength)
+                */
+                .transform(filterMap)
+                .flatMap(s -> splitString(s));
+    }
+
+    public Flux<String> namesFlux_defaultIfEmpty(int stringLength) {
+        return namesFlux_transform(stringLength).defaultIfEmpty("default");
+    }
+
+    public Flux<String> namesFlux_switchIfEmpty(int stringLength) {
+        Function<Flux<String>, Flux<String>> filterMap =
+                name -> name.map(s -> s.toUpperCase())
+                        .filter(s -> s.length() > stringLength)
+                        //Note that the flatmap has been moved here to make it common for default flux and return
+                        .flatMap(s -> splitString(s));
+
+        Flux<String> defaultFlux = Flux.just("default").transform(filterMap);
+
+        return namesFlux().transform(filterMap).switchIfEmpty(defaultFlux);
+
+    }
+
+    public Flux<String> explore_concat(){
+        Flux<String> abc=Flux.just("A","B","C");
+        Flux<String> def= Flux.just("D","E","F");
+
+        return Flux.concat(abc,def);
+    }
+
+    public Flux<String> explore_concatWith(){
+        Flux<String> abc=Flux.just("A","B","C");
+        Flux<String> def= Flux.just("D","E","F");
+
+        return abc.concatWith(def);
+    }
+
+    public Flux<String> explore_concatWithMono(){
+        Mono<String> a=Mono.just("A");
+        Mono<String> b= Mono.just("B");
+
+        return a.concatWith(b);
+    }
+
+    public Flux<String> explore_merge(){
+        Flux<String> abc=Flux.just("A","B","C").delayElements(Duration.ofMillis(100));
+        Flux<String> def= Flux.just("D","E","F").delayElements(Duration.ofMillis(125));
+
+        return Flux.merge(abc,def);
+    }
+
+    public Flux<String> explore_mergeWith(){
+        Flux<String> abc=Flux.just("A","B","C").delayElements(Duration.ofMillis(100));
+        Flux<String> def= Flux.just("D","E","F").delayElements(Duration.ofMillis(125));
+
+        return abc.mergeWith(def);
+    }
+
+    public Flux<String> explore_mergeWithMono(){
+        Mono<String> abc=Mono.just("A").delayElement(Duration.ofMillis(125));
+        Mono<String> def= Mono.just("B").delayElement(Duration.ofMillis(100));
+
+        return abc.mergeWith(def);
+    }
+
+    public Flux<String> explore_mergeSequential(){
+        Flux<String> abc=Flux.just("A","B","C").delayElements(Duration.ofMillis(125));
+        Flux<String> def= Flux.just("D","E","F").delayElements(Duration.ofMillis(100));
+
+        //Merge is sequential even if abc is having more delay than def
+        return Flux.mergeSequential(abc,def).log();
+    }
+
+    public Flux<String> explore_zip(){
+        Flux<String> abc=Flux.just("A","B","C");
+        Flux<String> def= Flux.just("D","E","F");
+
+        return Flux.zip(abc, def, (first,second) -> first+second);
+    }
+
+    public Flux<String> explore_zipTuples(){
+        Flux<String> abc=Flux.just("A","B","C");
+        Flux<String> def= Flux.just("D","E","F");
+
+        Flux<String> _123Flux=Flux.just("1","2","3");
+        Flux<String> _456Flux= Flux.just("4","5","6");
+
+        return Flux.zip(abc, def,_123Flux,_456Flux)
+                .map(t4 -> t4.getT1()+t4.getT2()+t4.getT3()+t4.getT4());
+    }
+
+    public Flux<String> explore_zipWith(){
+        Flux<String> abc=Flux.just("A","B","C");
+        Flux<String> def= Flux.just("D","E","F");
+
+        return abc.zipWith(def, (first,second) -> first+second);
+    }
+
+    public Mono<String> explore_zipWithMono(){
+        Mono<String> a=Mono.just("A");
+        Mono<String> b= Mono.just("B");
+
+        //This returns Mono
+        return a.zipWith(b)
+                .map(t2 -> t2.getT1()+t2.getT2());
+    }
+
+
+    private Flux<String> splitString(String name) {
+        return Flux.fromArray(name.split(""));
+    }
+
+    private Flux<String> splitStringWithDelay(String name) {
+        Random random = new Random();
+        int delay = random.nextInt(1000);
+        return Flux.fromArray(name.split("")).delayElements(Duration.ofMillis(delay));
+    }
+
+    private Mono<List<String>> splitStringMono(String s) {
+        return Mono.just(List.of(s.split("")));
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux().subscribe(System.out::println);
+        fluxAndMonoGeneratorService.nameMono().subscribe(System.out::println);
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,224 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(namesFlux)
+                //.expectNext("alex", "ben", "chloe")
+                //.expectNextCount(3)
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_map() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_map();
+
+        StepVerifier.create(namesFlux_map)
+                .expectNext("ALEX", "BEN", "CHLOE")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_immutability() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_immutability();
+
+        StepVerifier.create(namesFlux_map)
+                //.expectNext("ALEX", "BEN", "CHLOE")
+                .expectNext("alex", "ben", "chloe")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_filter() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_filter(3);
+
+        StepVerifier.create(namesFlux_map)
+                .expectNext("ALEX", "CHLOE")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_filterMapPipeline() {
+
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_filterMapPipeline(3);
+
+        StepVerifier.create(namesFlux_map)
+                .expectNext("4-ALEX", "5-CHLOE")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_flatMap() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_flatMap(3);
+
+        StepVerifier.create(namesFlux_map)
+                .expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_flatMapAsync() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_flatMapAsync(3);
+
+        //This did not fail as expected
+        StepVerifier.create(namesFlux_map)
+                //.expectNext("G", "A", "V", "A", "S", "K", "A", "R", "W", "A", "L", "S", "H", "R", "O", "B", "I", "N")
+                .expectNextCount(18)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_ConcatMap() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_ConcatMap(3);
+        StepVerifier.create(namesFlux_map)
+                .expectNext("G", "A", "V", "A", "S", "K", "A", "R", "W", "A", "L", "S", "H", "R", "O", "B", "I", "N")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMono_flatMap() {
+        Mono<List<String>> namesFlux_map = fluxAndMonoGeneratorService.namesMono_flatMap(3);
+        StepVerifier.create(namesFlux_map)
+                .expectNext(List.of("A", "L", "E", "X"))
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMono_flatMapMany() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesMono_flatMapMany(3);
+        StepVerifier.create(namesFlux_map)
+                .expectNext("A", "L", "E", "X")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_transform() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_transform(3);
+        StepVerifier.create(namesFlux_map)
+                .expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_defaultIfEmpty() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_defaultIfEmpty(5);
+        StepVerifier.create(namesFlux_map)
+                .expectNext("default")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_switchIfEmpty() {
+        Flux<String> namesFlux_map = fluxAndMonoGeneratorService.namesFlux_switchIfEmpty(5);
+        StepVerifier.create(namesFlux_map)
+                .expectNext("D","E","F","A","U","L","T")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_concat() {
+
+        Flux<String> abcdef= fluxAndMonoGeneratorService.explore_concat();
+        StepVerifier.create(abcdef)
+                .expectNext("A","B","C","D","E","F")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_concatWith() {
+
+        Flux<String> abcdef= fluxAndMonoGeneratorService.explore_concatWith();
+        StepVerifier.create(abcdef)
+                .expectNext("A","B","C","D","E","F")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_concatWithMono() {
+        Flux<String> ab= fluxAndMonoGeneratorService.explore_concatWithMono();
+        StepVerifier.create(ab)
+                .expectNext("A","B")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_merge() {
+        Flux<String> adbecf= fluxAndMonoGeneratorService.explore_merge();
+        StepVerifier.create(adbecf)
+                .expectNext("A","D","B","E","C","F")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_mergeWith() {
+        Flux<String> adbecf= fluxAndMonoGeneratorService.explore_mergeWith();
+        StepVerifier.create(adbecf)
+                .expectNext("A","D","B","E","C","F")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_mergeWithMono() {
+        Flux<String> ba= fluxAndMonoGeneratorService.explore_mergeWithMono();
+        StepVerifier.create(ba)
+                .expectNext("B","A")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_mergeSequential() {
+        Flux<String> abcdef= fluxAndMonoGeneratorService.explore_mergeSequential();
+        StepVerifier.create(abcdef)
+                .expectNext("A","B","C","D","E","F")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_zip() {
+        Flux<String> adbecf= fluxAndMonoGeneratorService.explore_zip();
+        StepVerifier.create(adbecf)
+                .expectNext("AD","BE","CF")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_zipTuples() {
+        Flux<String> t4= fluxAndMonoGeneratorService.explore_zipTuples();
+        StepVerifier.create(t4)
+                .expectNext("AD14","BE25","CF36")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_zipWith() {
+        Flux<String> adbecf= fluxAndMonoGeneratorService.explore_zipWith();
+        StepVerifier.create(adbecf)
+                .expectNext("AD","BE","CF")
+                .verifyComplete();
+    }
+
+    @Test
+    void explore_zipWithMono() {
+        Mono<String> ab= fluxAndMonoGeneratorService.explore_zipWithMono();
+        StepVerifier.create(ab)
+                .expectNext("AB")
+                .verifyComplete();
+    }
 }
